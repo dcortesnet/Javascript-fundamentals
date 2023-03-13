1754bfb07df65bab8ef9aa3de7b8cd91845dde11
dcortesnet <dcortes.net@gmail.com>
2023-03-13 13:14:27 -0300

    Update with dates operations

---
+++ 8. dates/date-methods.js
@@ -0,0 +1,10 @@
+const date = new Date();
+
+console.log(date.getDay());      // 1 (Lunes)
+console.log(date.getDate());     // 13 
+console.log(date.getMonth());    // 2 (Marzo)
+console.log(date.getHours());    // 12
+console.log(date.getMinutes());  // 40
+console.log(date.getSeconds());  // 51
+console.log(date.getFullYear()); // 2023
+console.log(date.getTime());     // 1678722750962
---
+++ 8. dates/date-operations.js
@@ -0,0 +1,7 @@
+
+const week = 1000 * 60 * 60 * 24 * 7;
+const date = new Date().getTime();
+
+const futureDateInNextWeek = new Date(date + week);
+
+console.log(futureDateInNextWeek); // 2023-03-20T15:54:38.565Z
