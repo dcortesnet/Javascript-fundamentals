ddf806449d69b896210945b5e8de92f81a6ee6fa
dcortesnet <dcortes.net@gmail.com>
2023-12-08 17:38:18 -0300

    Add operators

---
+++ 03. operators/03. comparisons/comparisons.js
@@ -0,0 +1,16 @@
+// >
+console.log(1 > 1);
+// >=
+console.log(1 >= 1);
+// <
+console.log(2 < 3);
+// <=
+console.log(2 <= 2);
+// ==
+console.log(2 == "2");
+// ====
+console.log(2 === "2");
+// !=
+console.log(1 != 2);
+// !==
+console.log(2 !== 2);
