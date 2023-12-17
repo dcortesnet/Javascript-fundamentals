88c291b4312fb11467430124634378d7e8d3a4f9
dcortesnet <dcortes.net@gmail.com>
2023-12-17 09:54:53 -0300

    Add samples

--- 08. dates/date.js
+++ 08. dates/date.js
@@ -1,14 +1,11 @@
 const date1 = new Date(); // Fri Mar 17 2023 11:01:03 GMT-0300 (hora de verano de Chile)
 date1.toISOString(); // 2023-03-17T14:01:03.031Z
 
-
 const date2 = new Date("2023/01/01 22:00:00"); // Sun Jan 01 2023 22:00:00 GMT-0300 (hora de verano de Chile)
 date2.toISOString(); // 2023-01-02T01:00:00.000Z
-
 
 const date3 = new Date(1678720536905); // Mon Mar 13 2023 12:15:36 GMT-0300 (hora de verano de Chile)
 date3.toISOString(); // 2023-03-13T15:15:36.905Z
 
-
-const date4 = new Date(2023, 03, 03, 17, 30, 00, 00); // Mon Apr 03 2023 17:30:00 GMT-0400 (hora estándar de Chile)
+const date4 = new Date(2023, 3, 3, 17, 30, 0, 0); // Mon Apr 03 2023 17:30:00 GMT-0400 (hora estándar de Chile)
 date4.toISOString(); // 2023-04-03T21:30:00.000Z
---
+++ 08. dates/exercises/README.md
@@ -0,0 +1,7 @@
+# Ejercicios date
+
+1. Obtener la fecha actual:Imprime la fecha actual en el formato "DD/MM/AAAA".
+2. Diferencia de días: Solicita al usuario ingresar dos fechas en formato "DD/MM/AAAA". Calcula la diferencia en días entre las dos fechas e imprime el resultado.
+3. Día de la semana: Pide al usuario ingresar una fecha en formato "DD/MM/AAAA". Imprime el día de la semana correspondiente a esa fecha.
+4. Sumar días a una fecha: Solicita al usuario ingresar una fecha en formato "DD/MM/AAAA". Pide al usuario ingresar un número de días.
+   Calcula la nueva fecha agregando esos días a la fecha ingresada e imprime el resultado.
